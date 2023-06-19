8ab7c964ee3d9d52b27fd9e942590fdd7146ee97
Fitra Muhammad Alfandy <fitramuhammad@ummi.ac.id>
2023-06-19 15:48:17 +0700

    upload file

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Fitra Muhammad A" // TODO: replace this
+const studentId = "FE5634740" // TODO: replace this
+
+export { url, studentName, studentId }
